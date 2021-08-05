9f51a0c7cf48dabc7533a342f3409a461189b174
Bryan Civils <hermanbciv@gmail.com>
2021-08-05 18:20:32 -0400

    Make minor updates while troubleshooting

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require ('path');
-const fs = require('fs');
+//const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000
-const router = express.Router()
+//const router = express.Router()
 const staticDirectoryPath = express.static(path.join(__dirname, './Develop/public'))
 //const staticDirectoryPath = express.static(path.join(__dirname, 'public', ));
 
