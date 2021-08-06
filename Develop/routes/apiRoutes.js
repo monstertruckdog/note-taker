b4c377d0199d3cad4d974fe9094d1fa7679f531c
Bryan Civils <hermanbciv@gmail.com>
2021-08-06 00:03:46 -0400

    Finalize changes after troubleshooting

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,5 @@
 const apiDb = require('../db/db.json')
 const fs = require('fs');
-// const router = require('express').Router();
 const uuid = require('uuid');
 
 
@@ -8,24 +7,11 @@ console.log('STARTING DATA')
 console.log(`LENGTH:  `, apiDb.length)
 console.log(apiDb)
 
-
-
 module.exports = (app) => {
   // READ NOTES
   app.get('/api/notes', (req, res) => {
-    // HOW TO REPRODUCE ISSUE:
-    // 1.  Un-comment any console.log inside this "READ NOTES" `app.get('/api/notes')`
-    // 2.  Run app
-    // 3.  Delete a note
-    // 4.  Refresh page
-    //       --> Error occurs
     console.log(`--> app.get starting`)
     fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
-      // console.log(`--> fs.readFile starting`)
-      //console.log(`--> fs.readFile data:  `, data)
-      // const name = 'noah'
-      // console.log(`TEST ${name}`)
-      // console.log(`--> fs.readFile with JSON.parse:  `, JSON.parse(data))
       let obj = JSON.parse(data);
       res.json(obj);
       })
@@ -50,28 +36,6 @@ module.exports = (app) => {
           res.sendStatus(200)
         }
     });
-    // --------------
-    // fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
-    //   if (err) {
-    //     console.log(`Error while attempting to read file -------->\n${err}`)
-    //   } else {
-    //       apiDb.push({
-    //         "id": uuid.v4(),
-    //         "title": req.body.title,
-    //         "text": req.body.text
-    //       })
-    //       fs.writeFile('./Develop/db/db.json', JSON.stringify(apiDb, undefined, 4), (err) => {
-    //           if (err) {
-    //             console.log(`Error writing file: ${err}`);
-    //             res.send(500)
-    //           } else {
-    //             console.log(`DATA SAVED SUCCESSFULLY`)
-    //             res.sendStatus(200)
-    //           }
-    //       });
-    //   }
-    // });
-    // -------------
   });
 
   // DELETE NOTE
@@ -87,16 +51,8 @@ module.exports = (app) => {
         console.log(`--> DELETE --> IN READ --> objectData:  `, objData);
         console.log(`--> DELETE --> IN READ --> req.body.id:  `,  req.body.id)
         console.log(`--> DELETE --> IN READ --> objData[0].id:  ${objData[0].id} <--> req.body.id:  ${req.body.id}`)
-        // let delNote = objData.splice([i], 1);
-        // fs.writeFile('./Develop/db/db.json', JSON.stringify(objData, undefined, 2), (err) => {
-        //   if (err) {
-        //     console.log(`Error writing file: `, err);
-        //   }
-        // })
-        // ---------
         console.log(`--> apiDb | 03 | before deletion:\nLENGTH:  `, apiDb.length, `\n`, apiDb)
-        for (let i = 0; i < objData.length; i++) {
-          
+        for (let i = 0; i < objData.length; i++) 
           if (objData[i].id === req.body.id) {
             console.log(`--> DELETE --> IN READ --> IN LOOP --> INDEX POSITION:  `, [i])
             console.log(`--> DELETE --> IN READ --> IN LOOP --> req.body.id:  `, req.body.id);
@@ -118,12 +74,6 @@ module.exports = (app) => {
               }
             })
           }
-            // -------
-        
-           // console.log = console.log(`--> DELETE --> IN READ --> IN LOOP --> FINAL ARRAY:  `, objData)
-          
-        }
-        // res.sendStatus(200)
       }
     });
   });
